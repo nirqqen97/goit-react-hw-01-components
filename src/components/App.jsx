bcaeed80a307b20cf290d74e47449e935790be2a
nirqqen97 <93587073+nirqqen97@users.noreply.github.com>
2022-12-16 17:36:38 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import {Profile} from './Profile/Profile';
 import { Statistic } from './Statistic/Statistic';
-import { FriendsList } from "./FriendsList/FriendsList";
+// import { FriendsList } from "./FriendsList/FriendsList";
 import { TransactionHistory } from './transactionHistory/transactionHistory';
 import user from '../data/user'
 import stats from "../data/data";
-import friends from '../data/friends';
+// import friends from '../data/friends';
 import transaction from "../data/transactions";
 export const App = () => {
   return (
@@ -16,7 +16,7 @@ export const App = () => {
       avatar={user.avatar}
       stats={user.stats}/>
     <Statistic stats ={stats}/>
-    <FriendsList friends = {friends}/>
+    {/* <FriendsList friends = {friends}/> */}
     <TransactionHistory transaction = {transaction}/>
     </div>
   );
